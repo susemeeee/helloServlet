5df70e3795ec28f9db5875de1b63849f6bca0e09
susemeeee <hffg123@naver.com>
2020-09-09 17:50:44 +0900

    Create the first servlet project

---
+++ src/ac/kr/hansung/LoginServlet.java
@@ -0,0 +1,53 @@
+package ac.kr.hansung;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		PrintWriter writer = response.getWriter();
+		
+		String htmlResponse = "<html>";
+		htmlResponse += "<h2> Your username is " + username + "<br/>";
+		htmlResponse += "<h2> Your password is " + password + "<br/>";
+		htmlResponse += "</html>";
+		
+		writer.println(htmlResponse);
+	}
+
+}
